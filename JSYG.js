12a727ba22e64a3f423eea7616869d0b5ddcccea
bochatayy <yannick.bochatay@meteo.fr>
2015-12-03 18:25:09 +0100

    Bug sur style2attr (il ne faut pas retirer l'attribut style de toutes les balises) et getDim.

--- JSYG.js
+++ JSYG.js
@@ -2244,7 +2244,8 @@
                 }
                 else {
                     
-                    box = this[0].getBBox();
+                    try { box = this[0].getBBox(); }
+                    catch(e) { return null; }
                     
                     dim = { //box est en lecture seule
                         x : box.x,
@@ -3969,7 +3970,9 @@
         
         if (standalone && this.isSVG()) {
             jNode.walkTheDom(function() {
-                new JSYG(this).style2attr().removeAttr("style");
+                var $this = new JSYG(this);
+                $this.style2attr();
+                if (JSYG.svgGraphics.indexOf($this.getTag()) != -1) $this.removeAttr("style");
             });
         }
         
